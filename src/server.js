096470fc347ed7a465bed6f86f53e8941802d6a6
hyzzzzy <shinhj4068@naver.com>
2023-05-29 16:25:03 +0900

    feat: room 구현

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import { WebSocketServer } from 'ws';
+import { Server } from "socket.io";
 
 const app = express();
 
@@ -15,11 +16,78 @@ app.get('/*', (req, res) => res.redirect('/'));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 // http 서버
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms }
+    }
+  } = wsServer;
+
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+
+  return publicRooms;
+}
+
+
+wsServer.on('connection', socket => {
+  socket['nickname'] = 'Anonymous';
+
+  socket.onAny(event => {
+    console.log(`Socket Event: ${event}`);
+    // rooms map => socket id와 방제를 가지고 있음
+    // sids map => socket id만 가지고 있음
+    // console.log(wsServer.sockets.adapter);
+  });
+  // custom event
+  socket.on('enter_room', (roomName, done) => {
+    // 기본적으로 user id와 user가 있는 room id와 같음
+    // User와 서버 사이에 private room이 존재
+    // console.log(socket.rooms);
+    // room에 들어감
+    socket.join(roomName);
+    // user id와 roomName 출력
+    // console.log(socket.rooms);
+    
+    done();
+    socket.to(roomName).emit('welcome', socket.nickname);
+    wsServer.sockets.emit('room_change', publicRooms());
+  });
+
+  // 페이지를 떠나기 직전
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach(room => {
+      socket.to(room).emit('bye', socket.nickname);
+    });
+  });
+
+  // 페이지를 떠난 후
+  socket.on('disconnect', () => {
+    wsServer.sockets.emit('room_change', publicRooms());
+  });
+
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
+    done();
+  });
+
+  socket.on('nickname', nickname => {
+    socket['nickname'] = nickname;
+  });
+});
+
 // websocket 서버
 // http, webSocket 둘다 작동시키는 코드
 // http 서버 위에 ws 서버를 만듦
-const wss = new WebSocketServer({ server });
+// const wss = new WebSocketServer({ httpServer });
+
 
 // socket: 연결된 어떤 사람, 서버와 브라우저 사이의 연결
 // 여기서의 socket은 연결된 브라우저
@@ -27,39 +95,39 @@ const handleConnection = (socket) => {
   console.log(socket);
 }
 
-const sockets = [];
+// const sockets = [];
 
-// 연결 리스너
-// callback으로 socket을 받음
-wss.on('connection', (socket) => {
-  // 연결된 socket을 배열에 담음
-  sockets.push(socket);
+// // 연결 리스너
+// // callback으로 socket을 받음
+// wss.on('connection', (socket) => {
+//   // 연결된 socket을 배열에 담음
+//   sockets.push(socket);
 
-  // 익명 유저
-  socket['nickname'] = 'Anonymous';
+//   // 익명 유저
+//   socket['nickname'] = 'Anonymous';
 
-  console.log('Connected to Browser');
+//   console.log('Connected to Browser');
 
-  socket.on('close', () => {
-    console.log('Disconnected from the Browser');
-  });
+//   socket.on('close', () => {
+//     console.log('Disconnected from the Browser');
+//   });
 
-  socket.on('message', (message) => {
-    const formattedMsg = Buffer.from(message, 'base64').toString('utf-8');
-    const parsedMsg = JSON.parse(formattedMsg);
+//   socket.on('message', (message) => {
+//     const formattedMsg = Buffer.from(message, 'base64').toString('utf-8');
+//     const parsedMsg = JSON.parse(formattedMsg);
 
-    switch(parsedMsg.type) {
-      case 'new_message': {
-        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${parsedMsg.payload}`));
-        break;
-      }
-      case 'nickname': {
-        socket['nickname'] = parsedMsg.payload;
-        break;
-      }
-    }
-  });
-});
+//     switch(parsedMsg.type) {
+//       case 'new_message': {
+//         sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${parsedMsg.payload}`));
+//         break;
+//       }
+//       case 'nickname': {
+//         socket['nickname'] = parsedMsg.payload;
+//         break;
+//       }
+//     }
+//   });
+// });
 
 // http, ws 같은 포트번호 사용
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
