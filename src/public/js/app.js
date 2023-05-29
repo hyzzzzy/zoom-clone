49d4216be8972001146c69028e55799ccb8a2b82
hyzzzzy <shinhj4068@naver.com>
2023-05-29 17:12:05 +0900

    feat: 사용자 카메라, 마이크 접근

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,90 +1,46 @@
 // socket.io를 실행하고 있는 서버를 찾음
 const socket = io();
+const myFace = document.getElementById('myFace');
+const muteBtn = document.getElementById('mute');
+const cameraBtn = document.getElementById('camera');
 
-const welcome = document.getElementById('welcome');
-const form = welcome.querySelector('form');
-const room = document.getElementById('room');
+let myStream;
+let muted = false;
+let cameraOff = false;
 
-room.hidden = true;
-let roomName = '';
-
-function addMessage(message) {
-  const ul = room.querySelector('ul');
-  const li = document.createElement('li');
-
-  li.innerText = message;
-  ul.appendChild(li);
-}
-
-function handleMessageSubmit(event) {
-  event.preventDefault();
-  const input = room.querySelector('#msg input');
-  const value = input.value;
-
-  socket.emit('new_message', input.value, roomName, () => {
-    addMessage(`You: ${value}`);
-  });
-
-  input.value='';
-}
-
-function handleNicknameSubmit(event) {
-  event.preventDefault();
-  const input = room.querySelector('#name input');
-
-  socket.emit('nickname', input.value);
-}
-
-function showRoom() {
-  welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector('h3');
-  h3.innerText = `Room ${roomName}`;
-
-  const msgForm = room.querySelector('#msg');
-  const nameForm = room.querySelector('#name');
-  
-  msgForm.addEventListener('submit', handleMessageSubmit);
-  nameForm.addEventListener('submit', handleNicknameSubmit);
-}
-
-function handleRoomSubmit(event) {
-  event.preventDefault();
-  const input = form.querySelector('input');
-
-  // custom event를 보냄
-  socket.emit('enter_room', input.value, showRoom);
-  roomName = input.value;
-  input.value='';
-}
-
-form.addEventListener('submit', handleRoomSubmit);
-
-socket.on('welcome', (user, newCount) => {
-  const h3 = room.querySelector('h3');
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMessage(`${user} joined`);
-});
-
-socket.on('bye', (left, newCount) => {
-  const h3 = room.querySelector('h3');
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMessage(`${left} left`);
-});
-
-socket.on('new_message', addMessage);
-
-socket.on('room_change', (rooms) => {
-  const roomList = welcome.querySelector('ul');
-  roomList.innerHTML = '';
-  
-  if (rooms.length === 0) {
-    return;
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch (e) {
+    console.error(e);
   }
+}
 
-  rooms.forEach(room => {
-    const li = document.createElement('li');
-    li.innerText = room;
-    roomList.append(li);
-  });
-});
+getMedia();
+
+function handleMuteClick() {
+  if (!muted) {
+    muteBtn.innerText = 'Unmute';
+    muted = true;
+  } else {
+    muteBtn.innerText = 'Mute';
+    muted = false;
+  }
+}
+
+function handleCameraClick() {
+  if (cameraOff) {
+    cameraBtn.innerText = 'Turn Camera Off';
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = 'Turn Camera On';
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener('click', handleMuteClick);
+cameraBtn.addEventListener('click', handleCameraClick);
